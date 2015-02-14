dee814e13649c0f25f985cc963654fc584cb096d
Wei Zhou <zoebbmm@gmail.com>
2015-02-14 17:58:29 -0500

    add helper()

--- helloWorld/src/NewClass.java
+++ helloWorld/src/NewClass.java
@@ -5,4 +5,8 @@ public class NewClass {
     public static void main(String[] args) {
         System.out.println("NewClass");
     }
+
+    public void helper(String s) {
+        s += "Yah!";
+    }
 }
