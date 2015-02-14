68518e922b4f723fb9be3dfd2abf4a99d9815b9b
Wei Zhou <zoebbmm@gmail.com>
2015-02-14 18:03:11 -0500

    uncomment str4

--- helloWorld/src/HelloWorld.java
+++ helloWorld/src/HelloWorld.java
@@ -13,7 +13,7 @@ public class HelloWorld {
 //
         String str3 = "%%%%%%%";
 //
-//        String str4 = "new test";
+        String str4 = "new test";
 
         String str5 = "123455";
         int value = 123455443;
