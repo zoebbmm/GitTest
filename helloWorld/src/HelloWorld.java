edcf4fd6440c5571019e2c3a37d4878ae554b248
Wei Zhou <zoebbmm@gmail.com>
2015-02-16 09:44:56 -0500

    change str5

--- helloWorld/src/HelloWorld.java
+++ helloWorld/src/HelloWorld.java
@@ -15,7 +15,7 @@ public class HelloWorld {
 //
         String str4 = "new test";
 
-        String str5 = "37777777";
+        String str5 = "sjdhdjdjdjsdsj";
         int value = 123455443;
         long l = (long) 23.2;
 
