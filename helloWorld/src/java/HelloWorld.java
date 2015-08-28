ee735df9c8fc27874370f242582d28101bd33a66
Wei Zhou <zoebbmm@gmail.com>
2015-08-28 09:31:17 -0400

    1st commit

---
+++ helloWorld/src/java/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Created by weizhou on 2/6/15.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        String str = "Wei Zhou";
+        System.out.println(str);
+    }
+}
---
+++ helloWorld/src/java/NewClass.java
@@ -0,0 +1,10 @@
+
+public class NewClass {
+    public static void main(String[] args) {
+        System.out.println("NewClass");
+    }
+
+    public void helper(String s) {
+        s += "Yah!";
+    }
+}
